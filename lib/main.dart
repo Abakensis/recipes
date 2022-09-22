0238c78c67de252e98bc6e214c2bca11e24ab583
Abakensis <98473957+Abakensis@users.noreply.github.com>
2022-09-22 18:19:36 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -85,16 +85,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-class RecipeDetail extends StatefulWidget {
-  final Recipe recipe;
-  const RecipeDetail({
-    Key? key,
-    required this.recipe,
-  }) : super(key: key);
-  @override
-  _RecipeDetailState createState() {
-    return _RecipeDetailState();
-  }
-}
-
