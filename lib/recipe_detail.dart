e38434564ff007fb51ae9fdc06114e96cdebe8e7
Abakensis <98473957+Abakensis@users.noreply.github.com>
2022-09-22 18:05:18 +0100

    Update recipe_detail.dart

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -14,7 +14,7 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
-  //todo : Add_sliverval here
+  int _sliderVal = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,8 +37,40 @@ class _RecipeDetailState extends State<RecipeDetail> {
             Text(
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
-              //todo:Add expanded
-              //todo : add slider here
+            ),
+
+            // 7
+            Expanded(
+// 8
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text('${ingredient.quantite * _sliderVal} '
+                      '${ingredient.mesure} '
+                      '${ingredient.nom}');
+                },
+              ),
+            ),
+            Slider(
+// 10
+              min: 1,
+              max: 10,
+              divisions: 10,
+// 11
+              label: '${_sliderVal * widget.recipe.parts} parts',
+// 12
+              value: _sliderVal.toDouble(),
+// 13
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+// 14
+              activeColor: Colors.green,
+              inactiveColor: Colors.white,
             ),
           ],
         ),
